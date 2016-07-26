04d6262eb61d3ce5fb19ec61ee2d757a155e0695
Mars <marshallmiller92@gmail.com>
2016-07-26 12:42:43 -0700

    changed setup for travis check

--- lab-Marshall/model/userConstruct.js
+++ lab-Marshall/model/userConstruct.js
@@ -3,9 +3,11 @@
 const appError = require('./appError');
 const uuid = require('node-uuid');
 
-const User = module.exports = function(name){
+const User = function(name){
   if(!name) return appError.error400();
   this.id = uuid.v1();
   this.name = name;
   this.date = Date.now();
 };
+
+module.exports = User;
