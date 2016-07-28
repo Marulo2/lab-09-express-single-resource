7f6d8818f4211d4521f7c90b446ed31605888091
Mars <marshallmiller92@gmail.com>
2016-07-28 10:43:52 -0700

    test server set up, one testing passing

--- lab-Marshall/test/test.js
+++ lab-Marshall/test/test.js
@@ -1,15 +1,40 @@
+'use strict';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
-
 const expect = chai.expect;
 const request = chai.request;
 
-const server = require('../server');
+const app = require('../server');
+let server;
 
-describe('empty test', () => {
-  it('should pass', () => {
 
+describe('CRUD TESTS', () => {
+  before((done) => {
+    server = app.listen(3000, () => {
+      console.log('up on 3000');
+      done();
+    });
   });
+
+  after((done) => {
+    server.close();
+    done();
+  });
+
+  it('should post a name', () => {
+    request('localhost:3000')
+      .post('/api/students')
+      .send({
+        name: 'marshall'
+      })
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.name).to.eql('marshall');
+      });
+  });
+
 });
