661caf2dfe379326d3195b0ea1f5224d26b39f10
Mars <marshallmiller92@gmail.com>
2016-07-28 15:26:49 -0700

    Working on middleware before I start mongod assignment

--- lab-Marshall/lib/appError.js
+++ lab-Marshall/lib/appError.js
@@ -1,27 +1,40 @@
 'use strict';
 
-const appError = module.exports = function(message, statusCode, resMessage){
-  this.message = message;
-  this.statusCode = statusCode;
-  this.resMessage = resMessage;
+
+
+
+let appError = module.exports = exports = function(statusCode, message, errCb) {
+  return function(error) {
+    errCb({error, statusCode, message, type: 'AppError'});
+  };
 };
 
-appError.prototype.respond = function(res){
-  res.status(this.statusCode).json(this.message);
-};
 
-appError.hasError = function(err){
-  return err instanceof appError;
-};
 
-appError.error400 = function(message){
-  return new appError(message, 400, 'Lame request!!!!');
-};
 
-appError.error404 = function(message){
-  return new appError(message, 404, 'not found!!!');
-};
+//
+// const appError = module.exports = function(message, statusCode, resMessage){
+//   this.message = message;
+//   this.statusCode = statusCode;
+//   this.resMessage = resMessage;
+// };
 
-appError.error500 = function(message){
-  return new appError(message, 500, 'server error!!!!');
-};
+// appError.prototype.respond = function(res){
+//   res.status(this.statusCode).json(this.message);
+// };
+//
+// appError.hasError = function(err){
+//   return err instanceof appError;
+// };
+//
+// appError.error400 = function(message){
+//   return new appError(message, 400, 'Lame request!!!!');
+// };
+//
+// appError.error404 = function(message){
+//   return new appError(message, 404, 'not found!!!');
+// };
+//
+// appError.error500 = function(message){
+//   return new appError(message, 500, 'server error!!!!');
+// };
--- lab-Marshall/server.js
+++ lab-Marshall/server.js
@@ -2,8 +2,58 @@
 
 const express = require('express');
 const app = express();
-const router = require('./route/route');
 
+const debug = require('debug');
+const serverLog = debug('cfdemo:serverLog');
+const serverErrLog = debug('cfdemo:serverError');
+
+const jsonParser = require('./lib/jsonParser');
+const bodyParser = require('body-parser');
+const appError = require('./lib/appError');
+
+const morgan = require('morgan');
+
+const router = require('./route/route');
 app.use('/api', router);
+
+// app.post('/somejson', jsonParser, (req, res) => {
+//   res.send(req.body);
+// });
+//
+// app.use((err, req, res, next) => {
+//   res.status(err.statusCode || 500).send(err.message);
+// });
+
+
+// app.use(morgan('combined', {
+//   skip: function(req, res) { return res.statusCode < 600})
+// });
+
+
+app.post('/api/students', jsonParser(401, 'crappy json'), (req, res, next) => {
+  serverLog(req.body);
+  res.send(':thumbsup:\n');
+});
+
+
+app.post('/bodyparser', bodyParser.json(), (req, res) => {
+  serverLog(req.body);
+  res.send('from body parser\n');
+});
+
+
+app.use((err, req, res, next) => {
+  if (err.type === 'appError') return next(err);
+  appError(500, '500: internal server error.', next)(err);
+});
+
+
+app.use((err, req, res, next) => {
+  serverErrLog(err.error.message);
+  res.status(err.statusCode).json({msg: err.message});
+});
+
+
+
 
 module.exports = app;
