e562691e2ac69e73fbf077914682ad3724027730
Mars <marshallmiller92@gmail.com>
2016-07-28 11:27:21 -0700

    post tests working, need help with get and put testing

--- lab-Marshall/test/test.js
+++ lab-Marshall/test/test.js
@@ -14,7 +14,7 @@ let server;
 describe('CRUD TESTS', () => {
   before((done) => {
     server = app.listen(3000, () => {
-      console.log('up on 3000');
+      console.log('Server up on 3000!');
       done();
     });
   });
@@ -24,7 +24,7 @@ describe('CRUD TESTS', () => {
     done();
   });
 
-  it('should post a name', () => {
+  it('should post a name', (done) => {
     request('localhost:3000')
       .post('/api/students')
       .send({
@@ -34,7 +34,31 @@ describe('CRUD TESTS', () => {
         expect(err).to.eql(null);
         expect(res).to.have.status(200);
         expect(res.body.name).to.eql('marshall');
+        done();
       });
   });
 
+  it('should return an error on a bad POST', (done) => {
+    request('localhost:3000')
+      .post('/api/students')
+      .send({
+        fake: 'fake'
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.eql('bad request');
+        done();
+      });
+  });
+
+  it('should get names', (done) => {
+    request(server)
+      .get('api/students')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.name).to.eql('marshall');
+        done();
+      });
+  });
 });
