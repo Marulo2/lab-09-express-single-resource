f8a8a473ce46c3d5fa54055a3b5ced88c2a64b3d
Mars <marshallmiller92@gmail.com>
2016-07-28 10:43:21 -0700

    route routing

--- lab-Marshall/route/route.js
+++ lab-Marshall/route/route.js
@@ -1,23 +1,40 @@
 'use strict';
 
 const Router = require('express').Router;
-let router = Router();
+const studentConstructor = require('../model/userConstruct');
+const appError = require('../lib/appError');
 
+const studentRegistry = {};
+const router = Router();
 var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/user/:id', (req, res) => {
-  res.status(200).json({id: req.params.id});
+router.get('/students/:id', (req, res) => {
+  if(!studentRegistry[req.params.id]){
+    return appError.error404('404 not found!!!').respond(res);
+  }
+  var students = studentRegistry[req.params.id];
+  return res.status(200).json(students);
 });
 
-router.post('/user', (req, res) => {
-  res.status(200).json({name: req.body.name});
+router.post('/students', jsonParser, (req, res) => {
+  if(!req.body.name){
+    return appError.error400('bad request').respond(res);
+  }
+  var students = new studentConstructor(req.body.name);
+  studentRegistry[students.id] = students;
+  res.status(200).json(students);
 });
 
-router.post('/user/:id', (req, res) => {
-  res.status(200).json({id: req.params.id});
+router.put('/students/:id', (req, res) => {
+  if(!req.params.id){
+    return appError.error404('404 not found!!!').respond(res);
+  }
+  var students = new studentConstructor(req.body.name);
+  studentRegistry[req.params.id] = students;
+  res.status(200).json(students);
 });
 
 module.exports = router;
