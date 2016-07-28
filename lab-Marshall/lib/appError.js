382dea0ef7ea854754fe68c47ba8baf8bd05a383
Mars <marshallmiller92@gmail.com>
2016-07-28 10:42:49 -0700

    apperror works

---
+++ lab-Marshall/lib/appError.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const appError = module.exports = function(message, statusCode, resMessage){
+  this.message = message;
+  this.statusCode = statusCode;
+  this.resMessage = resMessage;
+};
+
+appError.prototype.respond = function(res){
+  res.status(this.statusCode).json(this.message);
+};
+
+appError.hasError = function(err){
+  return err instanceof appError;
+};
+
+appError.error400 = function(message){
+  return new appError(message, 400, 'Lame request!!!!');
+};
+
+appError.error404 = function(message){
+  return new appError(message, 404, 'not found!!!');
+};
+
+appError.error500 = function(message){
+  return new appError(message, 500, 'server error!!!!');
+};
